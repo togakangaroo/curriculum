ff74ba8d5ab5d210873c8732ab821771840b55f8
dopeboy <manish@qhojo.com>
2014-08-23 19:10:11 -0400

    styling changes

---
+++ units/4-javascript/lessons/4-project/starter_code/js/guts.js
@@ -0,0 +1,31 @@
+function divide(x,y)
+{
+}
+
+function multiply(x,y)
+{
+}
+
+function subtract(x,y)
+{
+}
+
+function add(x,y)
+{
+}
+
+function power(x,y)
+{
+}
+
+function square_root(x)
+{
+}
+
+function logBase10(x)
+{
+}
+
+function generateRandomNumber()
+{
+}
